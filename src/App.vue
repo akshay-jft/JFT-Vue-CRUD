80bd1089d807716768007f3c8b4f934dd6589a63
akshay_2304 <toakshay.official@gmail.com>
2021-02-26 00:00:29 +0530

    initial Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="container-fluid px-0" id="body">
+  <Login/>
+</div>
+  
+  <router-view/>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+export default {
+  components : { Login }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap');
+  *{
+    font-family: 'Poppins', sans-serif;
+  }
+  :root {
+    --primary : #4db9a5;
+    --secondary : #f4f8f7;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"></link>
+
+createApp(App).use(router).mount('#app')
